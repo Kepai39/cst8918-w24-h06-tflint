215a8757d63a232c6f9f9cd02bc261e0ff57b1c8
Catherine Daigle(Kepai) <35710612+Kepai39@users.noreply.github.com>
2025-02-21 17:49:24 -0500

    fixed all variable names and descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,21 @@
 # Define output values for later reference
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
 }
 
 output "vm_name" {
   value = azurerm_linux_virtual_machine.webserver.name
+  description = "The name of the VM"
+
 }
 
 output "nic_name" {
   value = azurerm_network_interface.webserver.name
+  description = "The name of the NIC"
 }
 
 output "public_ip" {
   value = azurerm_linux_virtual_machine.webserver.public_ip_address
+  description = "The name of the public IP address"
 }
